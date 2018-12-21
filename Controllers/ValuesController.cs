50ee74f6c875dee8f940d57c9a74463cb54297bb
Devin Herath <devdhera@gmail.com>
2018-12-21 20:35:15 +0530

    Configured DB data population

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DateMaster.API.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DateMaster.API.Controllers
@@ -10,18 +11,28 @@ namespace DateMaster.API.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+
+        }
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public IActionResult GetValues()
         {
-            return new string[] { "value1", "value2" };
+            var values = _context.Values.ToList();
+
+            return Ok(values);
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public IActionResult GetValue(int id)
         {
-            return "value";
+            var value = _context.Values.FirstOrDefault(x => x.Id == id);
+
+            return Ok(value);
         }
 
         // POST api/values
